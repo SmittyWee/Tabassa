893d74817acd60c19c3367095baec9f48bb0e84a
callmeumm <developerroms@gmail.com>
2022-05-01 03:43:54 +0700

    [docs] prepare islamic-date

--- docs/pages/packages/islamic-date/index.tsx
+++ docs/pages/packages/islamic-date/index.tsx
@@ -1,9 +1,6 @@
 import { NextPage } from "next";
-import { HijrDate } from "@tabassa/islamic-date";
 
 const IslamicDatePackages: NextPage = () => {
-  console.log(new HijrDate(1443, 9, 30).gregorian);
-
   return <>
   
   </>
